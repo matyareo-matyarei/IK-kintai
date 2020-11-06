b4e28075dc7ae541aa90492f069451d042002e73
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-11-06 15:06:26 +0900

    attendancesコントローラー作成/ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "attendances#new"
+  resources :attendances, only: [:new, :create]
+
 end
