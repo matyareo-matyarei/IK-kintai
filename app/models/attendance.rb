d4dbd1b86fcf842f37e5f1c153ab251939a30e54
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-11-18 12:27:30 +0900

    エラーメッセージ時のレイアウト崩れ修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,17 +1,15 @@
 class Attendance < ApplicationRecord
-  with_options presence: true do
-    validates :work_days
-    validates :work_time
-  end
-  
-  validates :in_out, inclusion: { in: [true, false], message: 'を選んでください' }
-  validates :carfare, numericality: { only_integer: true, message: 'は半角数字で入力してください' }, allow_blank: true
-  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :work_place
   validates :work_place, presence: true
   validates :work_place_id, numericality: { other_than: 1, message: 'を選んでください' }
+
+  validates :work_days, presence: true
+  validates :work_time, presence: true
   
+  validates :in_out, inclusion: { in: [true, false], message: 'を選んでください' }
+  validates :carfare, numericality: { only_integer: true, message: 'は半角数字で入力してください' }, allow_blank: true
+    
   belongs_to :user
 
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module IKKintai
     # 日本語の言語設定
     config.i18n.default_locale = :ja
 
+    # エラーメッセージfield_with_errorsを自動挿入させない
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
