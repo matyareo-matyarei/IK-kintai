99784e64b540e5105a3f8f732d9531b930e7dbfe
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-11-22 21:36:48 +0900

    ユーザー編集機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :full_part, :affiliation_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :full_part, :affiliation_id])
   end
 end
