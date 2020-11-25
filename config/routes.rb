4e00655228d48e9b55a57174766413ab291ebb2a
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-11-25 18:30:15 +0900

    デザイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   get "users/show" => "users#show"
-
   root to: "attendances#new"
   resources :attendances, only: [:new, :create, :destroy] do
     collection do
@@ -11,5 +10,5 @@ Rails.application.routes.draw do
   post "/" => "attendances#create"
   get "/attendances" => "attendances#new"
   resources :users, only: :show
-
+  get "/users" => "attendances#new"
 end
