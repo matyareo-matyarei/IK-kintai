0b934d0944ddfa75c282eb6e7a490880d0d947f9
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2020-11-30 16:14:07 +0900

    他の月の入力はスプレッドシートに反映されないよう修正

--- spreadsheet/add_copy.rb
+++ spreadsheet/add_copy.rb
@@ -20,20 +20,22 @@ sheets = session.spreadsheet_by_key('17uvb27n9d-pobk0kVQy-YwJbXhxpEaOt2RBP0vB2j4
 
 # Google Drive上にディレクトリを作成する
 ## ディレクトリを作成する場所をGoogle DriveのURLで指定する(マイドライブのテストディレクトリ)
-drive_dir = session.collection_by_url('https://drive.google.com/drive/folders/1fTYhF5lR0l05GXWuQsMqsJ1YkvqdCmMn')
+# drive_dir = session.collection_by_url('https://drive.google.com/drive/folders/1fTYhF5lR0l05GXWuQsMqsJ1YkvqdCmMn')
 
 ## 上記で指定したディレクトリ内に新たにディレクトリを作成する
-drive_dir.create_subcollection('ディレクトリ名')
+# drive_dir.create_subcollection('ディレクトリ名')
+# drive_dir.create_subfolder('サブフォルダ')
 
 # スプレッドシートをコピーして指定したディレクトリに保存(移動)
 ## コピー元のスプレッドシートを取得
-sheets = session.spreadsheet_by_key('1F_3hgDzgPb0Eh9VD6aPPeVLn8a3OAonEH2kBFFp9h0Q')
+# sheets = session.spreadsheet_by_key('1iEssSEHR0FygCHRW0G22h9JOpo5eggewnkpFLyAjLF4')
+sheets = session.file_by_url('https://docs.google.com/spreadsheets/d/1FN3ZSIeLHFxPYMUa-XaqHj8JIP9DhTbkXP9_RiiRijQ/edit')
 
 ## シートのコピー
-copy_sheet = sheets.copy('コピー後のシート名_001')
+# copy_sheet = sheets.copy('コピー後のシート名_001')
 
 ## 保存したいディレクトリのURLを指定
 drive_dir = session.collection_by_url('https://drive.google.com/drive/folders/1Umuix7Wp_wlKSBXRq_xErsKMErS2YB3k')
 
-## コピーしたシートを先程指定したディレクトリ内に保存
-drive_dir.add(copy_sheet)
+## ()ファイルを先程指定したフォルダ（drive_dir）に移動
+drive_dir.add(sheets)
--- spreadsheet/destroy.rb
+++ spreadsheet/destroy.rb
@@ -1,54 +1,69 @@
 require 'google_drive'
 
 session = GoogleDrive::Session.from_config('config.json')
-# 書き込みたいスプレッドシートを、[$user.affiliation_id.name]で登録している所属院で入力する勤怠を指定する（浅草:2、 千束:3、 日暮里:4、 本部:5）
+# 勤怠入力の日付が2020年11月の場合
+if $attendance.work_days.year == 2020 && $attendance.work_days.month == 11
+  # 書き込むスプレッドシートを、[$user.affiliation_id]登録所属院で指定（浅草:2、 千束:3、 日暮里:4、 本部:5）
+  case $user.affiliation_id
+  when 2 # 浅草
+    spreads = session.spreadsheet_by_key('1c7my0A20BFbcG9hC62MQr1TkwEYj2wjLPeSa7BUio2Q')
+  when 3 # 千束
+    spreads = session.spreadsheet_by_key('1DgfB3Ayrk0QA7VUqeTO3P0aoL1pwgZKCPsnWmtHjLH8')
+  when 4 # 日暮里
+    spreads = session.spreadsheet_by_key('1_cLUUjCG-M16FvS9I_s9m7L6khCr4IjWNWmRc75zFZg')
+  when 5 # 本部
+    spreads = session.spreadsheet_by_key('1gKlozDb0P8VSYQw2wsR11JPgCtgVvvr-sbHvl45ShhI')
+  end
 
-case $user.affiliation_id
-when 2 # 浅草
-  spreads = session.spreadsheet_by_key('1c7my0A20BFbcG9hC62MQr1TkwEYj2wjLPeSa7BUio2Q')
-when 3 # 千束
-  spreads = session.spreadsheet_by_key('1DgfB3Ayrk0QA7VUqeTO3P0aoL1pwgZKCPsnWmtHjLH8')
-when 4 # 日暮里
-  spreads = session.spreadsheet_by_key('1_cLUUjCG-M16FvS9I_s9m7L6khCr4IjWNWmRc75zFZg')
-when 5 # 本部
-  spreads = session.spreadsheet_by_key('1gKlozDb0P8VSYQw2wsR11JPgCtgVvvr-sbHvl45ShhI')
+# 2020年12月の場合
+elsif $attendance.work_days.year == 2020 && $attendance.work_days.month == 12
+  case $user.affiliation_id
+  when 2 # 浅草
+    spreads = session.spreadsheet_by_key('')
+  when 3 # 千束
+    spreads = session.spreadsheet_by_key('')
+  when 4 # 日暮里
+    spreads = session.spreadsheet_by_key('')
+  when 5 # 本部
+    spreads = session.spreadsheet_by_key('')
+  end
+else
 end
+
 # [1DgfB3Ayrk0QA7VUqeTO3P0aoL1pwgZKCPsnWmtHjLH8]千束11月の勤怠
 # [1c7my0A20BFbcG9hC62MQr1TkwEYj2wjLPeSa7BUio2Q]浅草11月の勤怠
 # [1_cLUUjCG-M16FvS9I_s9m7L6khCr4IjWNWmRc75zFZg]日暮里11月の勤怠
 # [1gKlozDb0P8VSYQw2wsR11JPgCtgVvvr-sbHvl45ShhI]本部11月の勤怠
 
-# Userのfull_nameと一致するワークシートを取得
-sheet_tittle = $user.full_name
-sheet = spreads.worksheet_by_title(sheet_tittle)
+begin
+  
+  # Userのfull_nameと一致するワークシートを取得
+  sheet_tittle = $user.full_name
+  sheet = spreads.worksheet_by_title(sheet_tittle)
+  
+  # 出退勤記録の削除コマンド
+  # ***入力されている日付を基準にスプレットシートの入力する箇所を定める***[inputline]
+  inputline = $attendance.work_days.day + 5
+  
+  # 出勤(true)の場合／退勤(false)の場合を分岐
+  if $attendance.in_out
+    sheet[inputline, 3] = ""    # 出勤時間を削除
+  else
+    sheet[inputline, 4] = ""    # 退勤時間を削除
+  end
+  sheet[inputline, 8] = ""      # 勤務場所を削除
+  sheet[inputline, 10] = ""     # 交通費の入力の削除
 
-# 出退勤記録の削除コマンド
-## スプレッドシートへの""の上書き
-# ***入力されている日付を基準にスプレットシートの入力する箇所を定める***[inputline]
-inputline = $attendance.work_days.day + 5
+  if $user.full_part
+    sheet[inputline, 17] = ""   # 備考欄の削除（常勤）
+  else
+  sheet[inputline, 16] = ""     # 備考欄の削除（非常勤）
+  end
 
-# 出勤(true)の場合／退勤(false)の場合を分岐
-if $attendance.in_out
-  # 出勤時間を削除
-  sheet[inputline, 3] = ""
-else
-  # 退勤時間を削除
-  sheet[inputline, 4] = ""
+  sheet.save
+
+# 範囲外の日付指定の場合
+rescue StandardError => e
+  puts e.message
+  puts "正しくスプレッドシートには反映されていません"
 end
-
-# 勤務場所を削除
-sheet[inputline, 8] = ""
-
-# 交通費の入力の削除
-sheet[inputline, 10] = ""
-
-# 備考欄入力（あれば）
-if $user.full_part #常勤なら
-  sheet[inputline, 17] = ""
-else #非常勤なら
-  sheet[inputline, 16] = ""
-end
-
-
-sheet.save
-
