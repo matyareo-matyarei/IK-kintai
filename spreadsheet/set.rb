209f893a1bb3a94e2ea7c7c3d9a8d3ccac52dd17
matyareo-matyarei <masa.ahonya.yuka.ahonya@gmail.com>
2021-05-01 09:45:54 +0900

    5月対応

--- spreadsheet/set.rb
+++ spreadsheet/set.rb
@@ -23,6 +23,19 @@ if $attendance.work_days.year == 2021 && $attendance.work_days.month == 4
 elsif $attendance.work_days.year == 2021 && $attendance.work_days.month == 5
   case $user.affiliation_id
   when 2 # 浅草
+    key = '1EPaGc_yEBxEWZ_6LEGsb3PhopF1hkImzuKl2mf5Sofw'
+  when 3 # 千束
+    key = '1QQvo7UATOEqDDLyHxDJRm8-TyoPnFJ7gMU9ZxbwGOP4'
+  when 4 # 日暮里
+    key = '13Eg0WyNC8sHBLqSc6U-D1244ACTGKxKb5Q9cwEHk7W4'
+  when 5 # 本部
+    key = '1tSdYwz8UDWFCIzaHGBuXsDLgbCZE_zTAlylURKs4t7Y'
+  end
+
+  # 2021年6月の場合
+elsif $attendance.work_days.year == 2021 && $attendance.work_days.month == 6
+  case $user.affiliation_id
+  when 2 # 浅草
     key = ''
   when 3 # 千束
     key = ''
@@ -45,15 +58,15 @@ def thisMonth
   require 'holiday_japan'
   session = GoogleDrive::Session.from_config('config.json')
   case $user.affiliation_id
-    # 2021年3月のスプレッドシート設定
+    # 2021年4月のスプレッドシート設定
   when 2 # 浅草
-    key = '1K7qx0lzQQNIaQvdjP23tpxL-gqCai1ALNWpjmUYMQa0'
+    key = '1c3cmqFMBkmL1XEH6Vrj7ICaFC-jQfQwvRn99e1bYcYs'
   when 3 # 千束
-    key = '1JXy9oaZ2wzjDJGiYiux4up24KPH-AaWnmj6JM8OQiOM'
+    key = '1oXC_FZMMSujDiVJswCEZaTsQyJERmD6gImGwjcj3nQw'
   when 4 # 日暮里
-    key = '1ZHgTLkO_ZKu1rzvw8EyTxvMtYjbqihxPAr_pRO_nbLo'
+    key = '1ROu8pqYZ1XfXrPKoqm_USlnhhDkS9nJ0-KuLWqalW14'
   when 5 # 本部
-    key = '1H7BDBtesoOoDupZfjXMs2Ial25qwOL3NYq2xAp4HVTY'
+    key = '1t2P08n13clMPWgyrPAukFWw3UVZ2vfkcohCoNThFD7c'
   end      
   @sheet = session.spreadsheet_by_key(key).worksheet_by_title("#{$user.full_name}")
 end
